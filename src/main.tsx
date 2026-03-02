d6a282ff862c56d01fa208dcc4112b7dd7d23a17
geovaneoliveira97 <geovanebelotti2001@outlook.com>
2026-03-02 18:37:11 -0300

    debug: mostra erro na tela

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// 👇 SUBSTITUI o window.onerror anterior por isso
 window.onerror = (msg, src, line, col, err) => {
   document.body.innerHTML = `<pre style="color:red;padding:20px">${msg}\n${src}:${line}\n${err?.stack}</pre>`
+}
+
+window.onunhandledrejection = (e) => {
+  document.body.innerHTML = `<pre style="color:red;padding:20px">PROMISE REJEITADA:\n${e.reason?.stack || e.reason}</pre>`
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
