f39db119e94652060985267422e4f4faa50e9261
geovaneoliveira97 <geovanebelotti2001@outlook.com>
2026-03-02 18:34:20 -0300

    debug: mostra erro na tela

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+window.onerror = (msg, src, line, col, err) => {
+  document.body.innerHTML = `<pre style="color:red;padding:20px">${msg}\n${src}:${line}\n${err?.stack}</pre>`
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
